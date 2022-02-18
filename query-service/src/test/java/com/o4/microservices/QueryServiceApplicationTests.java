f1b0e9832325600d0eecbddf96ebdf6a5401701c
mazhar.hassan <netsurgeon@gmail.com>
2022-02-18 11:50:42 +0500

    Error handling for startup

--- query-service/src/test/java/com/o4/microservices/QueryServiceApplicationTests.java
+++ query-service/src/test/java/com/o4/microservices/QueryServiceApplicationTests.java
@@ -1,13 +1,6 @@
 package com.o4.microservices;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class QueryServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
 }
+
